0f9a2e3a3057a61b42c474bc8ec9c1331d4ca528
Brent Peterson <brentp24@gmail.com>
2020-05-05 15:22:42 -0600

    inquirer

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,15 @@
 // TODO: Write code to define and export the Employee class
+// const employee = new Employee;
+
+//   class Employee {
+//       constructor(employeeName, uniqueID, email) {
+//         this.employeeName = employeeName;
+//         this.uniqueID = uniqueID;
+//         this.email = email;
+//       }
+
+//     }
+
+// //   const employee = new Employee(response.employeeName, response.uniqueID, response.email);
+
+// module.exports = Employee;
